6982814bc46b5499fa769eed0f6f09d95a5d3f1f
영인 <youngin@yeong-in-ui-MacBookPro.local>
2023-04-19 16:25:32 +0900

    commit message

---
+++ pages/1_Webcam.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, ClientSettings
+
+# Function to display the webcam feed
+def main():
+    st.title("Real-time Webcam Video")
+    
+    st.markdown("## Webcam Video")
+    webrtc_ctx = webrtc_streamer(key="example", video_transformer_factory=None)
+    
+    if webrtc_ctx.video_transformer:
+        st.markdown("Stream is running")
+    else:
+        st.markdown("Starting stream...")
+
+
+if __name__ == "__main__":
+    main()
