427ef91d231ecd69c5fee9cb662c26740db73a87
youngin2 <younginchoi7@gmail.com>
2023-04-27 01:20:08 +0900

    .

--- pages/5_Webcam_with_YOLOv5_apple.py
+++ pages/5_Webcam_with_YOLOv5_apple.py
@@ -51,15 +51,11 @@ st.markdown("Click the 'Start' button below to access your webcam and see the ob
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
-
 token = client.tokens.create()
-
-
-
 
 webrtc_ctx = webrtc_streamer(key="YOLOv5", 
                             mode=WebRtcMode.SENDRECV,
